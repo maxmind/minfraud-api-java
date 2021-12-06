ab2db083321e2847df573c33c496218c78e2af02
Gregory Oschwald <goschwald@maxmind.com>
2021-12-06 08:48:55 -0800

    Do not create ObjectMapper repeatedly

--- src/main/java/com/maxmind/minfraud/AbstractModel.java
+++ src/main/java/com/maxmind/minfraud/AbstractModel.java
@@ -1,30 +1,33 @@
 package com.maxmind.minfraud;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
 
 public abstract class AbstractModel {
+    private final static ObjectMapper mapper = JsonMapper.builder()
+            .addModule(new JavaTimeModule())
+            .defaultDateFormat(new StdDateFormat().withColonInTimeZone(true))
+            .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
+            .disable(MapperFeature.CAN_OVERRIDE_ACCESS_MODIFIERS)
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .serializationInclusion(JsonInclude.Include.NON_NULL)
+            .serializationInclusion(JsonInclude.Include.NON_EMPTY)
+            .build();
 
     /**
      * @return JSON representation of this object.
      * @throws IOException if there is an error serializing the object to JSON.
      */
     public final String toJson() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-        mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
-        mapper.disable(MapperFeature.CAN_OVERRIDE_ACCESS_MODIFIERS);
-        mapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
         return mapper.writeValueAsString(this);
     }
 
