933553d8aed66fc5e3693cd6d861b4f3631e1a13
Gregory Oschwald <goschwald@maxmind.com>
2022-12-12 12:41:48 -0800

    Use correct module name

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.maxmind.db {
+module com.maxmind.minfraud {
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
